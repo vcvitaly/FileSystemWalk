663f0b6476b702899768b277c58a8efdd05b5043
Vitalii Chura <vcvitaly@yahoo.com>
2023-05-09 03:28:02 +0300

    Implement delete

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	root := flag.String("root", ".", "Root directory to start")
 	// Action options
 	list := flag.Bool("list", false, "List files only")
+	del := flag.Bool("del", false, "Delete files")
 	// Filter options
 	ext := flag.String("ext", "", "File extension to filter out")
 	size := flag.Int64("size", 0, "Minimum file size")
@@ -22,6 +23,7 @@ func main() {
 		ext:  *ext,
 		size: *size,
 		list: *list,
+		del:  *del,
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
@@ -45,6 +47,12 @@ func run(root string, out io.Writer, cfg config) error {
 				return listFile(path, out)
 			}
 
+			// Delete files
+			if cfg.del {
+				return delFile(path)
+			}
+
+			// List is the default option if nothing else was set
 			return listFile(path, out)
 		})
 }
@@ -56,4 +64,6 @@ type config struct {
 	size int64
 	// list files
 	list bool
+	// delete files
+	del bool
 }
